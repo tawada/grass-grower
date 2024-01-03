fb09e35398c31fadc9cf9d1e6420cbc38934c201
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-01-03 13:04:46 +0000

    Use runtime args

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from routers import update_issue
+import sys
+from routers import (
+    generate_readme,
+    update_issue,
+)
 
-
-def main():
-    update_issue()
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv
+    if len(args) > 1:
+        if args[1] == "generate_readme":
+            generate_readme()
+        elif args[1] == "update_issue":
+            update_issue()
