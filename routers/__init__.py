fd6591c22f12e8cb2bf74ee42c5e133d14f6f432
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-01-04 10:26:30 +0000

    Add a code generator from an issue

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,8 +3,53 @@ from pathlib import Path
 from services import (
     generate_text,
     get_issue,
+    get_issue_by_id,
     reply_issue,
 )
+
+
+def generate_code_from_issue(issue_id: int):
+    """issueからコードを生成する"""
+
+    issue = get_issue_by_id(issue_id)
+    if issue is None:
+        print("issueが取得できませんでした")
+        return
+
+    # issueの内容からどのようなコードを生成するかを決定する
+    messages = []
+
+    # 既存の拡張子.pyのファイルをすべて列挙する
+    for file in Path(".").glob("**/*.py"):
+        with open(file, "r") as f:
+            message = "```" + file.name + "\n"
+            message += f.read() + "```"
+            messages.append(
+                {
+                    "role": "user",
+                    "content": message,
+                }
+            )
+
+    # issueをpromptに追加する
+    message = "```" + issue.title + "\n"
+    message += issue.body + "```\n"
+    messages.append(
+        {
+            "role": "user",
+            "content": message,
+        }
+    )
+    message = "You are a programmer of the highest caliber.Please read the code of the existing program and rewrite any one based on the issue. You must output the file name on the first row and whole code on the second row and below."
+    messages.append(
+        {
+            "role": "system",
+            "content": message,
+        }
+    )
+
+    generated_text = generate_text(messages)
+    print(generated_text)
 
 
 def update_issue():
