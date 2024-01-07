77c5a51312d65e2a62ffefbbcc041e485c24a07e
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-01-07 10:17:46 +0000

    Add Python application CI workflow and tests

---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+from main import parse_arguments
+
+
+def test_parse_arguments_valid():
+    args = ["add_issue", "--issue-id", "123"]
+    parsed_args = parse_arguments(args)
+    assert parsed_args.action == "add_issue"
+    assert parsed_args.issue_id == 123
