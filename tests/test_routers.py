7396e61296223e8a24db3ed3e98481abfad30089
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-03-02 11:50:18 +0000

    Update file I/O mock in test_routers.py

--- tests/test_routers.py
+++ tests/test_routers.py
@@ -11,20 +11,8 @@ def test_add_issue(
 ):
     """Test add_issue() function."""
     setup(mocker)
-
-    class DummyFileController:
-        """Dummy file controller class."""
-
-        def __enter__(self):
-            return type("FileController", (object, ), {
-                "read": lambda: "test",
-                "write": lambda *args: True
-            })
-
-        def __exit__(self, *args):
-            pass
-
-    mocker.patch("logic.open", return_value=DummyFileController())
+    mocker.patch("builtins.open",
+                 return_value=utils.mock_open_with(read_data="test"))
     routers.add_issue("test_owner/test_repo", "python")
 
 
@@ -35,39 +23,16 @@ def test_update_issue(
     """Test update_issue() function."""
     setup(mocker)
 
-    class DummyFileController:
-        """Dummy file controller class."""
-
-        def __enter__(self):
-            return type("FileController", (object, ), {
-                "read": lambda: "test",
-                "write": lambda *args: True
-            })
-
-        def __exit__(self, *args):
-            pass
-
-    mocker.patch("logic.open", return_value=DummyFileController())
+    mocker.patch("builtins.open",
+                 return_value=utils.mock_open_with(read_data="test"))
     routers.update_issue(1, "test_owner/test_repo", "main", "python")
 
 
 def test_generate_code_from_issue(mocker, setup):
     """Test generate_code_from_issue() function."""
     setup(mocker)
-
-    class DummyFileController:
-        """Dummy file controller class."""
-
-        def __enter__(self):
-            return type("FileController", (object, ), {
-                "read": lambda: "test",
-                "write": lambda *args: True
-            })
-
-        def __exit__(self, *args):
-            pass
-
-    mocker.patch("logic.open", return_value=DummyFileController())
+    mocker.patch("builtins.open",
+                 return_value=utils.mock_open_with(read_data="test"))
     routers.generate_code_from_issue(1, "test_owner/test_repo", "main",
                                      "python")
 
@@ -89,20 +54,8 @@ def test_generate_readme(mocker, setup):
 def test_generate_readme_failed(mocker, setup):
     """Test generate_readme() function."""
     setup(mocker)
-
-    class DummyFileController:
-        """Dummy file controller class."""
-
-        def __enter__(self):
-            return type("FileController", (object, ), {
-                "read": lambda: "test",
-                "write": lambda *args: True
-            })
-
-        def __exit__(self, *args):
-            pass
-
-    mocker.patch("routers.open", return_value=DummyFileController())
+    mocker.patch("builtins.open",
+                 return_value=utils.mock_open_with(read_data="test"))
     routers.generate_readme("test_owner/test_repo", "main", "python")
 
 
@@ -125,20 +78,8 @@ def test_grow_grass_yesterday(mocker, setup):
 def test_generate_code_from_issue_and_reply(mocker, setup):
     """Test generate_code_from_issue_and_reply() function."""
     setup(mocker)
-
-    class DummyFileController:
-        """Dummy file controller class."""
-
-        def __enter__(self):
-            return type("FileController", (object, ), {
-                "read": lambda: "test",
-                "write": lambda *args: True
-            })
-
-        def __exit__(self, *args):
-            pass
-
-    mocker.patch("logic.open", return_value=DummyFileController())
+    mocker.patch("builtins.open",
+                 return_value=utils.mock_open_with(read_data="test"))
     mocker.patch("services.llm.generate_json",
                  return_value={
                      "filepath": "test_filepath",
