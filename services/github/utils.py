32633659e9a15dcdf46bb8faeb49d39073ac0e59
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-03-13 13:39:33 +0000

    Delete make_owner_dir function from utils.py

--- services/github/utils.py
+++
@@ -1,9 +0,0 @@
-"""Utility functions for the GitHub API."""
-import os
-
-
-def make_owner_dir(base_path: str, repo: str):
-    """Create a directory for the repository owner."""
-    owner = repo[:repo.index("/")]
-    path = os.path.join(base_path, owner)
-    os.makedirs(path, exist_ok=True)
