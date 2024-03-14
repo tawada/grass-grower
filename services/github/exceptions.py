57109475292c9053ccfa8632033ff424559d30cc
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-03-14 01:21:58 +0000

    Add GitNothingToCommitException to exception_keywords

--- services/github/exceptions.py
+++ services/github/exceptions.py
@@ -26,6 +26,11 @@ class GitBranchAlreadyExistsException(GitException):
     message = "already exists"
 
 
+class GitNothingToCommitException(GitException):
+    """Exception raised for errors in the Git API."""
+    message = "nothing to commit, working tree clean"
+
+
 class GitHubException(CommandExecutionException):
     """Base exception raised for errors in the GitHub API."""
 
@@ -42,8 +47,8 @@ class GitHubRepoNotFoundException(GitHubException):
 
 exception_keywords: dict[str, type[CommandExecutionException]] = {}
 _src: list[type[CommandExecutionException]] = [
-    GitBranchAlreadyExistsException, GitHubConnectionException,
-    GitHubRepoNotFoundException
+    GitBranchAlreadyExistsException, GitNothingToCommitException,
+    GitHubConnectionException, GitHubRepoNotFoundException
 ]
 for _exception in _src:
     exception_keywords[_exception.message] = _exception
