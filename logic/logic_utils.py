22b6b369e079e630b3ab6440db6d56ccb9c241e2
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-03-22 14:43:41 +0000

    Refactor directory and file filtering logic

--- logic/logic_utils.py
+++ logic/logic_utils.py
@@ -9,12 +9,18 @@ def enumarate_file_paths(repo_path: str, target_extension: list[str]):
 
     for root, dirs, files in os.walk(repo_path):
         # Limit the directories to explore
-        dirs[:] = [
-            d for d in dirs
-            if d not in config["exclude_dirs"] and not d.startswith(".")
-            if not d.startswith(".") and not d.startswith("_")
-            and not d.startswith("venv")
-        ]
+        dirs[:] = [d for d in dirs if is_target_dir(d)]
         for file in files:
-            if file.endswith(tuple(target_extension)):
+            if is_target_file(file, target_extension):
                 yield os.path.join(root, file)
+
+
+def is_target_dir(dir_name: str):
+    """Check if the directory is not a target directory."""
+    return dir_name not in config["exclude_dirs"] and not dir_name.startswith(
+        ".")
+
+
+def is_target_file(file_path: str, target_extension: list[str]):
+    """Check if the file is a target file."""
+    return file_path.endswith(tuple(target_extension))
