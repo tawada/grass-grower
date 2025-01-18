8ad2857ee6c05f4114e959ec9951203eccf2074e
tawada <tawada@togawa.cs.waseda.ac.jp>
2025-01-18 14:55:01 +0000

    Update default path with environment variable in config loader

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,11 +1,13 @@
 """Module for loading configuration from a JSON file."""
 
 import json
+import os
+from typing import Any, Dict
 
 from utils.logging_utils import log
 
 
-def load_config(file_path="config.json"):
+def load_config(file_path: str = "") -> Dict[str, Any]:
     """Load configuration from a JSON file.
 
     Args:
@@ -14,6 +16,8 @@ def load_config(file_path="config.json"):
     Returns:
         dict: Configuration as a dictionary.
     """
+    if file_path == "":
+        file_path = os.getenv('CONFIG_PATH', 'config.json')
     try:
         with open(file_path) as config_file:
             return json.load(config_file)
@@ -25,16 +29,18 @@ def load_config(file_path="config.json"):
         return get_default_config()
     except json.JSONDecodeError as err:
         log(
-            f"Error decoding JSON from {file_path}. Details: {err.msg} at line {err.lineno}, column {err.colno}",
+            (f"Error decoding JSON from {file_path}. "
+             f"Details: {err.msg} at line {err.lineno}, column {err.colno}"),
             level="error",
         )
         return get_default_config()
 
 
-def get_default_config():
+def get_default_config() -> Dict[str, Any]:
     """Provide a default configuration as a fallback."""
+    repository_path = os.getenv('REPOSITORY_PATH', 'downloads')
     return {
-        "repository_path": "downloads",
-        "exclude_dirs": ["__pycache__", ".git", "downloads"],
-        "openai_model_name": "gpt-4o",
+        "repository_path": repository_path,
+        "exclude_dirs": ["__pycache__", ".git", repository_path],
+        "openai_model_name": os.getenv('OPENAI_MODEL_NAME', 'gpt-4'),
     }
